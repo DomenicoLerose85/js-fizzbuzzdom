0baca8a6e7402a81cdb81245408aea16f49f9255
DomenicoLerose85 <domenicolerose85@libero.it>
2022-03-28 16:12:54 +0200

    end_of_excercise

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
+let printingSelector = document.querySelector('.stamp');
+
+
 for (let index = 1; index < 101; index++) {
     console.log(index);
     if (index % 3 === 0) {
-       index = 'fizz';
-       console.log(index); 
+        console.log('fizz');
     } else if (index % 5 === 0) {
-        index = 'buzz'; 
+        console.log('buzz');
+    } else if (index % 3 === 0 && index % 5 === 0) {
+        console.log('fizzbuzz');
     }
     
+    let element = `<p>${index}</p>`;
+ 
+    printingSelector.innerHTML += element;
 }
